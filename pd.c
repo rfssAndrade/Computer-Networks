6023b5813574b2c4c0f048ec1e3a188544ee2cdf
Ricardo Andrade <ricardoandrade@MacBook-Pro-4.lan>
2020-11-01 10:57:12 +0000

    fixes

--- pd.c
+++ pd.c
@@ -56,7 +56,7 @@ void parseArgs(int argc, char **argv) {
 
 
 void makeConnection() {
-    int fd, errcode = 0, out_fds;
+    int fd_client, fd_server, errcode = 0, out_fds;
     ssize_t n;
     socklen_t addrlen;
     struct addrinfo hints_as, hints_pd, *res_as, *res_pd;
@@ -64,18 +64,20 @@ void makeConnection() {
     char buffer[MAX_INPUT], command[5], second[6], third[9], answer[128], message[42]; //change answer
     fd_set inputs, testfds;
 
-    fd = socket(AF_INET, SOCK_DGRAM, 0);
-    if (fd == -1) exit(1);  // correto?
+    fd_client = socket(AF_INET, SOCK_DGRAM, 0);
+    if (fd_client == -1) exit(1);  // correto?
+
+    fd_server = socket(AF_INET, SOCK_DGRAM, 0);
+    if (fd_server == -1) exit(1);
 
     FD_ZERO(&inputs);
     FD_SET(0, &inputs);
-    FD_SET(fd, &inputs);
+    FD_SET(fd_client, &inputs);
+    FD_SET(fd_server, &inputs);
 
     memset(&hints_as, 0, sizeof hints_as);
     hints_as.ai_family = AF_INET;
     hints_as.ai_socktype = SOCK_DGRAM;
-
-    //addr.sin_port = htons(atoi(PDport));
 
     errcode = getaddrinfo(ASIP, ASport, &hints_as, &res_as);
     if (errcode != 0) exit(1); // correto?
@@ -84,10 +86,11 @@ void makeConnection() {
     hints_pd.ai_family = AF_INET;
     hints_pd.ai_socktype = SOCK_DGRAM;
     hints_pd.ai_flags = AI_PASSIVE;
+
     errcode = getaddrinfo(PDIP, PDport, &hints_pd, &res_pd);
     if (errcode != 0) exit(1);
 
-    while (bind(fd,res_pd->ai_addr,res_pd->ai_addrlen) ==  -1) puts("Can't bind");
+    while (bind(fd_server,res_pd->ai_addr,res_pd->ai_addrlen) ==  -1) puts("Can't bind");
         
     while (1) {
         testfds = inputs;
@@ -116,21 +119,26 @@ void makeConnection() {
                     if (errcode == REG) sprintf(message, "REG %s %s %s %s\n", second, third, PDIP, PDport);
                     else sprintf(message, "UNR %s %s\n", uid, pass);
 
-                    n = sendto(fd, message, strlen(message), 0, res_as->ai_addr, res_as->ai_addrlen); // pode ser blocking?
+                    n = sendto(fd_client, message, strlen(message), 0, res_as->ai_addr, res_as->ai_addrlen); // pode ser blocking?
                     if (n == ERROR) puts("ERROR");//?????
                 }
-                else if (FD_ISSET(fd, &testfds)) {
+                else if (FD_ISSET(fd_client, &testfds)) {
                     addrlen = sizeof(addr);
-                    n = recvfrom(fd, answer, 128, 0, (struct sockaddr *)&addr, &addrlen);
+                    n = recvfrom(fd_client, answer, 128, 0, (struct sockaddr *)&addr, &addrlen);
                     if (n == ERROR) puts("ERROR");//??????
                     
+                    verifyAnswer(answer);
+                }
+
+                else if (FD_ISSET(fd_server, &testfds)) {
+                    addrlen = sizeof(addr);
+                    n = recvfrom(fd_server, answer, 128, 0, (struct sockaddr *)&addr, &addrlen);
+                    if (n == ERROR) puts("ERROR");//??????
+
                     n = verifyAnswer(answer);
-                    if (n == 2) {
-                        sprintf(message, "RVC OK\n");
-                        printf("%s", message);
-                        n = sendto(fd, message, strlen(message), 0, (struct sockaddr *)&addr, addrlen); //mudar
-                        if (n == ERROR) puts("ERROR");
-                    }
+                    sprintf(message, "RVC OK\n");
+                    n = sendto(fd_server, message, strlen(message), 0, (struct sockaddr *)&addr, addrlen); //mudar
+                    if (n == ERROR) puts("ERROR");
                     // falta para RVC NOK
                 }
                 break;
@@ -140,7 +148,8 @@ void makeConnection() {
 
     freeaddrinfo(res_as);
     freeaddrinfo(res_pd);
-    close(fd);
+    close(fd_client);
+    close(fd_server);
 }
 
 
