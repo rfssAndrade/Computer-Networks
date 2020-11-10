8f32108b8f6727bfb2d7e567f0219036fbbca7ae
danielquintas8 <daniel.quintas.lopes@gmail.com>
2020-11-10 00:10:41 +0000

    Update pd.c

--- pd.c
+++ pd.c
@@ -161,12 +161,7 @@ void makeConnection() {
                         sprintf(message, "RVC %s NOK\n", uid);
                         printf("Bad message: %s\n", answer);
                     }
-                        int code;
-    socklen_t addrlen = sizeof(addr_server);
-
-    code = sendto(fd_server, message, strlen(message), 0, (struct sockaddr *)&addr_server, addrlen); //mudar
-    if (code == ERROR) puts("ERROR");
-                    // sendMessageServer(fd_server, message, addr_server);
+                    sendMessageServer(fd_server, message, addr_server);
                 }
                 break;
         }
